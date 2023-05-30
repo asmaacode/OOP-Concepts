97caddf8b802fd07a9ce2af911ea6dab0c622bd5
Asma'a <78433466+asmaacode@users.noreply.github.com>
2023-05-30 18:55:08 +0300

    Update clsDate.h

--- clsDate.h
+++ clsDate.h
@@ -4,7 +4,27 @@
 #include <vector>
 #include <iostream>
 #include <string>
+#include<iomanip>
 #include "clsString.h"
+class clsDraw {
+public:
+	static string generateLine(short length = 0, char symbol = '_') {
+		string line = "";
+		while (length != 0) {
+			line += symbol;
+			length--;
+		}
+		return line;
+	};
+	static string generateTabs(short length = 0) {
+		string tabs = "";
+		while (length != 0) {
+			tabs += " ";
+			length--;
+		}
+		return tabs;
+	};
+};
 class clsDate
 {
 private:
@@ -91,7 +111,78 @@ public:
 		}
 		return date;
 	};
-	
+	string getMonthName(short& month) {
+		string monthsNames[] = { "","Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec" };
+		return monthsNames[month];
+	};
+	string getWeekDayName(short dayOrder) {
+		string weekDays[] = { "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" };
+		return weekDays[dayOrder];
+	}
+	short getOrderWeekDayOfDate(short day, short month, short year) {
+		short a = floor((14 - month) / 12);
+		short y = year - a;
+		short m = month + 12 * a - 2;
+		short d = day + y + floor(y / 4) - floor(y / 100) + floor(y / 400) + floor(31 * m / 12);
+		//short d = 5 + day + y + floor(y / 4) + floor(31 * m / 12);
+		d = d % 7;
+		return d;
+	}
+	short getOrderWeekDayOfDate(clsDate date) {
+		return getOrderWeekDayOfDate(date._day, date._month, date._year);
+	}
+	void printHeader(short& month) {
+		cout << "\n" << clsDraw::generateTabs(2);
+		cout << clsDraw::generateLine(15) << getMonthName(month) << clsDraw::generateLine(15) << "\n\n";
+		cout << clsDraw::generateTabs(2);
+		cout << setw(3) << getWeekDayName(0) << "  ";
+		cout << setw(3) << getWeekDayName(1) << "  ";
+		cout << setw(3) << getWeekDayName(2) << "  ";
+		cout << setw(3) << getWeekDayName(3) << "  ";
+		cout << setw(3) << getWeekDayName(4) << "  ";
+		cout << setw(3) << getWeekDayName(5) << "  ";
+		cout << setw(3) << getWeekDayName(6) << "  ";
+		cout << endl;
+
+	}
+	void printFooter() {
+		cout << "\n" << clsDraw::generateTabs(2);
+		cout << clsDraw::generateLine(18) << clsDraw::generateLine(15) << "\n\n";
+	}
+	void printBody(short& year, short& month) {
+		short firstDay = getOrderWeekDayOfDate(1, month, year);
+		short countMonthDays = countDaysInMonth(year, month);
+		short currentDay = 1;
+
+		cout << clsDraw::generateTabs(2);
+		for (short i = 0;currentDay <= countMonthDays;i++) {
+			if (i >= firstDay) {
+				cout << setw(3) << currentDay << "  ";
+				currentDay++;
+			}
+			else {
+				cout << setw(3) << " " << "  ";
+			}
+
+			if ((i + 1) % 7 == 0) {
+				cout << endl;
+				cout << clsDraw::generateTabs(2);
+			}
+		}
+	}
+	void printMonthCalendar(short year, short month) {
+		printHeader(month);
+		printBody(year, month);
+		printFooter();
+	}
+	void printYearCalendar() {
+		cout << clsDraw::generateTabs(2) << clsDraw::generateLine(33, '=') << "\n";
+		cout << clsDraw::generateTabs(12) << "Calendar - " << _year << "\n";
+		cout << clsDraw::generateTabs(2) << clsDraw::generateLine(33, '=') << "\n";
+		for (short i = 1;i <= 12;i++) {
+			printMonthCalendar(_year, i);
+		};
+	};
 	clsDate() {
 		_day = getThisDay();
 		_month = getThisMonth();
